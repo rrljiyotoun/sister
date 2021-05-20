0d5d5654688c99ca70a588202d6c4ae29b07ddd0
rrljiyotoun <yichuji1983329@126.com>
2021-05-20 18:25:24 +0800

    百思不得姐 项目创建

---
+++ 百思不得姐/百思不得姐/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  百思不得姐
+//
+//  Created by 杨庆 on 2016/12/30.
+//  Copyright © 2016年 杨庆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 百思不得姐/百思不得姐/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  百思不得姐
+//
+//  Created by 杨庆 on 2016/12/30.
+//  Copyright © 2016年 杨庆. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	NSLog(@"OSChina ");
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+
+@end
